3dd6f2dd2340c1b54c7dfafa1f1597533d116165
musaab27 <musaab1996@gmail.com>
2023-11-28 17:51:12 -0800

    Build Image metadata to mint NFT
    Build Image metadata to mint NFT

--- ts/cluster1/nft_metadata.ts
+++ ts/cluster1/nft_metadata.ts
@@ -1,7 +1,12 @@
-import wallet from "../wba-wallet.json"
-import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
-import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi"
-import { createBundlrUploader } from "@metaplex-foundation/umi-uploader-bundlr"
+import wallet from './wba-wallet.json';
+import { createUmi } from '@metaplex-foundation/umi-bundle-defaults';
+import {
+  createGenericFile,
+  createSignerFromKeypair,
+  signerIdentity,
+} from '@metaplex-foundation/umi';
+import { createBundlrUploader } from '@metaplex-foundation/umi-uploader-bundlr';
+import { PublicKey } from '@solana/web3.js';
 
 // Create a devnet connection
 const umi = createUmi('https://api.devnet.solana.com');
@@ -13,33 +18,31 @@ const signer = createSignerFromKeypair(umi, keypair);
 umi.use(signerIdentity(signer));
 
 (async () => {
-    try {
-        // Follow this JSON structure
-        // https://docs.metaplex.com/programs/token-metadata/changelog/v1.0#json-structure
+  try {
+    // Follow this JSON structure
+    // https://docs.metaplex.com/programs/token-metadata/changelog/v1.0#json-structure
+    const image =
+      'https://arweave.net/EJYVSTZ-odsekSDC77HCW0zU6B3_lCYHgmWqs97K35A';
+    const metadata = {
+      name: 'Musaab Exotic Rug',
+      symbol: 'MER',
+      description: 'WBA RUG',
+      image: image,
+      attributes: [{ trait_type: 'RugType', value: 'Exotic' }],
+      properties: {
+        files: [
+          {
+            type: 'image/png',
+            uri: '?',
+          },
+        ],
+      },
+      creators: [{ address: keypair.publicKey, share: 100 }],
+    };
 
-        // const image = ???
-        // const metadata = {
-        //     name: "?",
-        //     symbol: "?",
-        //     description: "?",
-        //     image: "?",
-        //     attributes: [
-        //         {trait_type: '?', value: '?'}
-        //     ],
-        //     properties: {
-        //         files: [
-        //             {
-        //                 type: "image/png",
-        //                 uri: "?"
-        //             },
-        //         ]
-        //     },
-        //     creators: []
-        // };
-        // const myUri = ???
-        // console.log("Your image URI: ", myUri);
-    }
-    catch(error) {
-        console.log("Oops.. Something went wrong", error);
-    }
+    const myUri = await bundlrUploader.uploadJson(metadata);
+    console.log('Your image URI: ', myUri);
+  } catch (error) {
+    console.log('Oops.. Something went wrong', error);
+  }
 })();
